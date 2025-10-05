202365bcc8fc6e8ba0c1c57bd4484de3df320df0
sheema-sumayya <syed.sumayya@wipro.com>
2025-10-05 21:39:16 +0530

    Add correct index.js content

--- .github/actions/joke-action/index.js
+++ .github/actions/joke-action/index.js
@@ -1,1 +1,18 @@
-
+const core = require('@actions/core');
+const request = require('request-promise');
+async function run(){
+  try{
+    const name=core.getInput('name')||'friend';
+    const res = await request({
+      uri:
+        ''https://official-joke-api.appspot.com/jokes/random',
+    json:true,
+      });
+  const joke= res && res.setup&& res.punchline ? '${res.setup}-${res.punchline}' : 'No joke found';
+  core.info('Hello ${name}! Here's a joke:${joke}');
+    core.setOutput('joke',joke);
+}catch (eoor){
+  core.setFailed(error.message);
+}
+}
+run();
